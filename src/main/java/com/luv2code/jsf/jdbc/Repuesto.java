2239878545c5d7336117620d0339b85f3bef4ad3
lcorrea <lcorrea@indracompany.com>
2023-07-24 09:10:27 -0500

    agregando ejemplo jsf con primefaces

---
+++ src/main/java/com/luv2code/jsf/jdbc/Repuesto.java
@@ -0,0 +1,102 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.luv2code.jsf.jdbc;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author lacorrea
+ */
+public class Repuesto implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private Integer pkId;
+
+    private String nombre;
+
+    private String descripcion;
+
+    private Date fechaRegistro;
+
+    private TipoRepuesto tipoRepuesto;
+
+    public Repuesto() {
+    }
+
+    public Repuesto(Integer pkId) {
+        this.pkId = pkId;
+    }
+
+    public Integer getPkId() {
+        return pkId;
+    }
+
+    public void setPkId(Integer pkId) {
+        this.pkId = pkId;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Date getFechaRegistro() {
+        return fechaRegistro;
+    }
+
+    public void setFechaRegistro(Date fechaRegistro) {
+        this.fechaRegistro = fechaRegistro;
+    }
+
+    public TipoRepuesto getTipoRepuesto() {
+        return tipoRepuesto;
+    }
+
+    public void setTipoRepuesto(TipoRepuesto tipoRepuesto) {
+        this.tipoRepuesto = tipoRepuesto;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (pkId != null ? pkId.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Repuesto)) {
+            return false;
+        }
+        Repuesto other = (Repuesto) object;
+        if ((this.pkId == null && other.pkId != null) || (this.pkId != null && !this.pkId.equals(other.pkId))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "be.Repuesto[ pkId=" + pkId + " ]";
+    }
+    
+}
---
+++ src/main/java/com/luv2code/jsf/jdbc/TipoRepuestoBean.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.luv2code.jsf.jdbc;
+
+import java.io.Serializable;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Date;
+import javax.faces.bean.ManagedBean;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+import org.primefaces.PrimeFaces;
+
+/**
+ *
+ * @author lacorrea
+ */
+@ManagedBean
+@SessionScoped
+public class TipoRepuestoBean implements Serializable{
+
+    private TipoRepuesto objeto;
+    private TipoRepuestoDbUtil tipoRepuestoDbUtil;
+    private List<TipoRepuesto> tiporepuestos;
+    private int tipoAccion = 1;
+    
+    public TipoRepuestoBean() throws Exception{
+        reinstanciar();
+        tiporepuestos = new java.util.LinkedList<TipoRepuesto>();
+        tipoRepuestoDbUtil = TipoRepuestoDbUtil.getInstance();
+	}
+    
+    public List<TipoRepuesto> getLista() throws Exception{
+        return tipoRepuestoDbUtil.getTipoRepuestos();
+    }
+
+    public TipoRepuesto getObjeto() {
+        return objeto;
+    }
+
+    public void setObjeto(TipoRepuesto objeto) {
+        this.objeto = objeto;
+    }
+
+    public int getTipoAccion() {
+        return tipoAccion;
+    }
+
+    public void setTipoAccion(int tipoAccion) {
+        this.tipoAccion = tipoAccion;
+    }
+    
+    public void reinstanciar(){
+        //Crear
+        objeto = new TipoRepuesto();
+    }
+    
+    public void procesar(){
+        //Crear
+        objeto.setFechaRegistro(new Date());
+        //this.tipoRepuestoFacade.create(objeto);
+    }
+    
+}
